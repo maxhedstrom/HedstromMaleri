6bf88170ebf56290206270a4f8e83936d133456a
maxhedstrom <132456905+maxhedstrom@users.noreply.github.com>
2025-04-02 17:42:23 +0200

    Gemensamma färger tillagda

--- src/pages/Hem.jsx
+++ src/pages/Hem.jsx
@@ -12,7 +12,7 @@ const Hem = () => {
       ></section>
 
       <div className="w-[90%] lg:w-3/5 text-white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
-        <h1 className="text-[32px] md:text-[42px] lg:text-[42px]">
+        <h1 className=" text-[var(--first-color)] text-[32px] md:text-[42px] lg:text-[42px]">
           Ett litet familjeföretag med fokus på kvalité och nöjda kunder.
         </h1>
         <p className="mt-[10px] mb-[40px] text-[18px] md:text-[24px] lg:text-[20px]">
@@ -27,28 +27,28 @@ const Hem = () => {
       </div>
       {/*Presentation med urval av tjänster i röda block - sektion: Tjänster */}
       <section className="w-4/5 mx-auto text-center pt-[100px]">
-        <h1 className="text-4xl font-semibold">Tjänster vi erbjuder</h1>
-        <p className="text-[#333] text-[14px] font-light leading-[22px] p-[10px]">
+        <h1 className="text-[var(--rubrik-color)] text-4xl font-semibold">Tjänster vi erbjuder</h1>
+        <p className="text-[var(--text-color)] text-[14px] font-light leading-[22px] p-[10px]">
           Vi erbjuder ett antal olika tjänster
         </p>
         <div className="mt-[5%] flex flex-col md:flex-row justify-between">
           <div className="basis-[31%] bg-[#fff3f3] rounded-[10px] mb-[5%] py-5 px-3 hover:shadow-[0_0_20px_0_rgba(0,0,0,0.2)]">
-            <h3 className="text-center font-semibold my-[10px]">Spackling</h3>
-            <p>
+            <h3 className="text-[var(--rubrik-color)] text-center font-semibold my-[10px]">Spackling</h3>
+            <p className="text-[var(--text-color)]">
             Ge din fasad ett lyft med professionell målning! Vi på Hedström Måleri AB använder moderna metoder och lift för att nå 
             alla ytor smidigt och säkert. Med högkvalitativa färger och noggrant arbete skyddar vi din fasad och ger den ett fräscht, hållbart utseende.
             </p>
           </div>
           <div className="basis-[31%] bg-[#fff3f3] rounded-[10px] mb-[5%] py-5 px-3 hover:shadow-[0_0_20px_0_rgba(0,0,0,0.2)]">
-            <h3 className="text-center font-semibold my-[10px]">Fasadmålning</h3>
-            <p>
+            <h3 className="text-[var(--rubrik-color)] text-center font-semibold my-[10px]">Fasadmålning</h3>
+            <p className="text-[var(--text-color)]">
             Ge din fasad ett lyft med professionell målning! Vi på Hedström Måleri AB använder moderna metoder och lift för att nå 
             alla ytor smidigt och säkert. Med högkvalitativa färger och noggrant arbete skyddar vi din fasad och ger den ett fräscht, hållbart utseende.
             </p>
           </div>
           <div className="basis-[31%] bg-[#fff3f3] rounded-[10px] mb-[5%] py-5 px-3 hover:shadow-[0_0_20px_0_rgba(0,0,0,0.2)]">
-            <h3 className="text-center font-semibold my-[10px]">Våtrum</h3>
-            <p>
+            <h3 className="text-[var(--rubrik-color)] text-center font-semibold my-[10px]">Våtrum</h3>
+            <p className="text-[var(--text-color)]">
             Behöver ditt badrum eller liknande målas om krävs specialverktyg och kompetens kring hållbarhet. Våra våtrumstjänster garanterar hållbara, stilrena och fuktsäkra lösningar för ditt badrum eller tvättstuga. 
             Vi använder högkvalitativa material och följer branschstandarder för att skapa en säker och elegant miljö 
             </p>
@@ -58,8 +58,8 @@ const Hem = () => {
 
       {/*Bild på Örebro slott med lite text om företagets geografi - sektion: Geografisk plats */}
       <section className="w-4/5 mx-auto text-center pt-[100px]">
-        <h1 className="text-4xl font-semibold">Var vi finns</h1>
-        <p className="text-[#333] text-[14px] font-light leading-[22px] p-[10px]">
+        <h1 className="text-[var(--rubrik-color)] text-4xl font-semibold">Var vi finns</h1>
+        <p className="text-[var(--text-color)] text-[14px] font-light leading-[22px] p-[10px]">
         Hedström Måleri AB är baserat i Örebro, ett penseldrag från slottet. Vi utför arbeten i hela Närke med omnejd. 
        
         Vi erbjuder våra tjänster i både större städer och mindre orter i regionen.
@@ -84,8 +84,8 @@ const Hem = () => {
 
       <section className="w-4/5 mx-auto text-center pt-[100px]">
         <div className="mb-8">
-          <h1 className="text-4xl font-semibold">Utvalda uppdragsgivare</h1>
-          <p className="text-[#333] text-[14px] font-light leading-[22px] p-[10px]">
+          <h1 className="text-[var(--rubrik-color)] text-4xl font-semibold">Utvalda uppdragsgivare</h1>
+          <p className="text-[var(--text-color)] text-[14px] font-light leading-[22px] p-[10px]">
             Vi utför arbeten åt kunder, stora som små, och strävar alltid efter att leverera högsta kvalitet.
             <br />
             Vi har ett antal referenser!
@@ -100,7 +100,7 @@ const Hem = () => {
                 className="w-full h-[200px] object-contain rounded-[10px]"
               />
             </a>
-            <h3 className="mt-[10px] mb-[15px]">Örebro Kommun</h3>
+            <h3 className="text-[var(--text-color)] mt-[10px] mb-[15px]">Örebro Kommun</h3>
           </div>
           <div className="rounded-[10px] text-center">
             <a href="https://www.oru.se/">
@@ -110,7 +110,7 @@ const Hem = () => {
                 className="w-full h-[200px] object-contain rounded-[10px]"
               />
             </a>
-            <h3 className="mt-[10px] mb-[15px]">Sörbyskolan</h3>
+            <h3 className="text-[var(--text-color)] mt-[10px] mb-[15px]">Sörbyskolan</h3>
           </div>
           <div className="rounded-[10px] text-center">
             <a href="https://www.oru.se/">
@@ -120,7 +120,7 @@ const Hem = () => {
                 className="w-full h-[200px] object-contain rounded-[10px]"
               />
             </a>
-            <h3 className="mt-[10px] mb-[15px]">Örebro Universitet</h3>
+            <h3 className="text-[var(--text-color)] mt-[10px] mb-[15px]">Örebro Universitet</h3>
           </div>
         </div>
       </section>
