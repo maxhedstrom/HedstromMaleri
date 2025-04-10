d8278bccebdb0d7f8bcc4036b70c446f29fb64c1
maxhedstrom <max.hedstrom@icloud.com>
2025-04-10 13:32:59 +0200

    ny mapp data skapad

---
+++ src/data/services.js
@@ -0,0 +1,19 @@
+  const services = [
+    {
+      title: "Spackling",
+      description:
+        "Ge din fasad ett lyft med professionell målning! Vi på Hedström Måleri AB använder moderna metoder och lift för att nå alla ytor smidigt och säkert. Med högkvalitativa färger och noggrant arbete skyddar vi din fasad och ger den ett fräscht, hållbart utseende.",
+    },
+    {
+      title: "Fasadmålning",
+      description:
+        "Ge din fasad ett lyft med professionell målning! Vi på Hedström Måleri AB använder moderna metoder och lift för att nå alla ytor smidigt och säkert. Med högkvalitativa färger och noggrant arbete skyddar vi din fasad och ger den ett fräscht, hållbart utseende.",
+    },
+    {
+      title: "Våtrum",
+      description:
+        "Behöver ditt badrum eller liknande målas om krävs specialverktyg och kompetens kring hållbarhet. Våra våtrumstjänster garanterar hållbara, stilrena och fuktsäkra lösningar för ditt badrum eller tvättstuga. Vi använder högkvalitativa material och följer branschstandarder för att skapa en säker och elegant miljö.",
+    },
+  ];
+
+  export default services;
--- src/pages/Hem.jsx
+++ src/pages/Hem.jsx
@@ -4,6 +4,7 @@ import "../styles/hem.css";
 import ServiceCard from "/src/components/ui/ServiceCard.jsx";
 // import ServiceCard from "@/src/components/ui/ServiceCard";
 // import ServiceCard from "@/components/ui/ServiceCard";
+import services from "../data/services";
 
 
 const Hem = () => {
@@ -24,23 +25,7 @@ const Hem = () => {
   //       "Behöver ditt badrum eller liknande målas om krävs specialverktyg och kompetens kring hållbarhet. Våra våtrumstjänster garanterar hållbara, stilrena och fuktsäkra lösningar för ditt badrum eller tvättstuga. Vi använder högkvalitativa material och följer branschstandarder för att skapa en säker och elegant miljö.",
   //   },
   // ];
-  const services = [
-    {
-      title: "Spackling",
-      description:
-        "Ge din fasad ett lyft med professionell målning! Vi på Hedström Måleri AB använder moderna metoder och lift för att nå alla ytor smidigt och säkert. Med högkvalitativa färger och noggrant arbete skyddar vi din fasad och ger den ett fräscht, hållbart utseende.",
-    },
-    {
-      title: "Fasadmålning",
-      description:
-        "Ge din fasad ett lyft med professionell målning! Vi på Hedström Måleri AB använder moderna metoder och lift för att nå alla ytor smidigt och säkert. Med högkvalitativa färger och noggrant arbete skyddar vi din fasad och ger den ett fräscht, hållbart utseende.",
-    },
-    {
-      title: "Våtrum",
-      description:
-        "Behöver ditt badrum eller liknande målas om krävs specialverktyg och kompetens kring hållbarhet. Våra våtrumstjänster garanterar hållbara, stilrena och fuktsäkra lösningar för ditt badrum eller tvättstuga. Vi använder högkvalitativa material och följer branschstandarder för att skapa en säker och elegant miljö.",
-    },
-  ];
+
 
   
   
