046675941364572969ccd774ae412c5f560e3f07
maxhedstrom <132456905+maxhedstrom@users.noreply.github.com>
2025-05-15 16:46:47 +0200

    nytt lösen

--- src/generateHash.js
+++
@@ -1,9 +0,0 @@
-// generateHash.js
-import bcrypt from 'bcrypt';
-
-const plainPassword = "h11";
-const saltRounds = 10;
-
-bcrypt.hash(plainPassword, saltRounds).then(hash => {
-  console.log("Hash:", hash);
-});
