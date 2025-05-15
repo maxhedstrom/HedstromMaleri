9e4ed3e0775b3a9f8eb7cfd5ad43e179f1813ef6
maxhedstrom <132456905+maxhedstrom@users.noreply.github.com>
2025-05-15 16:37:42 +0200

    import fungerar

---
+++ src/generateHash.js
@@ -0,0 +1,9 @@
+// generateHash.js
+import bcrypt from 'bcrypt';
+
+const plainPassword = "h11";
+const saltRounds = 10;
+
+bcrypt.hash(plainPassword, saltRounds).then(hash => {
+  console.log("Hash:", hash);
+});
